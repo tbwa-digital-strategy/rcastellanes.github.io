5223b0004f0bffc6345f9fba058e81533cf4c985
Richie <richcast@gmail.com>
2015-04-25 17:59:51 -0400

    Update mapleaflet.js

--- mapleaflet.js
+++ mapleaflet.js
@@ -1,12 +1,28 @@
+// See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
+
 var map = L.map( 'map', {
-    center: [40.7260493,-73.9917831],
-    minZoom: 7,
-    zoom: 13
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2
 });
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v4/rcastellanes.d1bd2cf7/{z}/{x}/{y}.png', {
+L.tileLayer( 'http://{s}.tiles.mapbox.com/v4/rcastellanes.d1bd2cf7/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicmNhc3RlbGxhbmVzIiwiYSI6IkVRd0dDaVEifQ.EsWXaa4BxE2qtGQ658rXqQ', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">MapBox</a>'
-    subdomains: ['otile1','otile2','otile3','otile4']
 }).addTo( map );
 
 var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+
+var myIcon = L.icon({
+    iconUrl: myURL + 'images/pin24.png',
+    iconRetinaUrl: myURL + 'images/pin48.png',
+    iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14]
+});
+
+for ( var i=0; i < markers.length; ++i ) 
+{
+   L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+      .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+      .addTo( map );
+}
