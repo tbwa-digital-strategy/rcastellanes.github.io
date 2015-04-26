58b577e97ea05fec2a2262c2c7cd4db12b65c884
Richie <richcast@gmail.com>
2015-04-26 15:36:13 -0400

    Update mapleaflet.js

--- mapleaflet.js
+++ mapleaflet.js
@@ -1,9 +1,9 @@
 // See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
 
 var map = L.map( 'map', {
-    center: [40.7260493,-73.9917831],
-    minZoom: 13,
-    zoom: 2
+    center: [0,0],
+    minZoom: 4,
+    zoom: 18
 });
 
 L.tileLayer( 'http://{s}.tiles.mapbox.com/v4/rcastellanes.d1bd2cf7/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicmNhc3RlbGxhbmVzIiwiYSI6IkVRd0dDaVEifQ.EsWXaa4BxE2qtGQ658rXqQ', {
@@ -12,10 +12,12 @@ L.tileLayer( 'http://{s}.tiles.mapbox.com/v4/rcastellanes.d1bd2cf7/{z}/{x}/{y}.p
 
 var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
 
-var marker = new L.RegularPolygonMarker(new L.LatLng(40.7260493,-73.9917831), {
+var marker = new L.RegularPolygonMarker(new L.LatLng(0, 0), {
     numberOfSides: 3,
     rotation: 60.0,
-    radius: 10
+    radius: 10,
+    // L.Path style options
+    ...
 });
 
 map.addLayer(marker);
